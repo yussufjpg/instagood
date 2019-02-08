d36db330d0ddbe4b4c4455fd4fb7924bec5b0d01
gustavopradoreis <henrique.gustavoreis@gmail.com>
2019-02-08 07:57:17 -0200

    update README and package.json

--- example.js
+++ example.js
@@ -19,3 +19,15 @@ user.do('follow', 'reidarking').then((response) => console.log(response), (err) 
 // Unfollow
 
 user.do('unfollow', 'reidarking').then((response) => console.log(response), (err) => console.log(err));
+
+// Like
+
+user.likes('like', '1973268968068413381').then((response) => console.log(response), (err) => console.log(err));
+
+// Unlike
+
+user.likes('unlike', '1973268968068413381').then((response) => console.log(response), (err) => console.log(err));
+
+// Comment
+
+user.say('1973450160415933226', 'I liked!').then((response) => console.log(response), (err) => console.log(err));
